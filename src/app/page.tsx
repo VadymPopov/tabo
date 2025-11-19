dc54195af19e4fde0d1d71ad1a7849cf172294cc
VadymPopov <boycristal10@gmail.com>
2025-11-19 18:19:14 -0500

    fix: images names

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,10 +88,10 @@ export default function HomePage() {
       <section className="px-6 py-20 max-w-5xl mx-auto text-center">
         <h3 className="text-3xl font-bold mb-10">Organizations We Serve</h3>
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-12 items-center justify-center">
-          <Image src="/bay-hoops.png" alt="logo" width={100} height={100} />
+          <Image src="/Bay-hoops.png" alt="logo" width={100} height={100} />
           <Image src="/BWT.png" alt="logo" width={100} height={100} />
           <Image src="/osba.svg" alt="logo" width={100} height={100} />
-          <Image src="/D-league.png" alt="logo" width={100} height={100} />
+          <Image src="/D-League.png" alt="logo" width={100} height={100} />
           <Image src="/CISAA.png" alt="logo" width={100} height={100} />
           <Image src="/SSAF.png" alt="logo" width={100} height={100} />
           <Image src="/TDSB.png" alt="logo" width={100} height={100} />
